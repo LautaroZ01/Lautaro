d71b5ca6129b8f6ae377d8bdaa35bf3249e99a0e
Lautaro Zuleta <lautarozule80@gmail.com>
2024-04-30 11:29:13 -0300

    Correccion para el envio de correos

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
     const form = useRef();
-    const userEmai = useRef({
-        user: '',
-        emailUser: '',
-        mensaje: ''
-    })
 
     const sendEmail = (e) => {
         const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
